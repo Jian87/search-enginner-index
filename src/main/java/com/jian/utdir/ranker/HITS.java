58d31b2aa9e60690846b0268e0ed000c014b7f88
Jian87 <wangjian4814@gmail.com>
2021-04-09 22:36:10 -0500

    'm'

--- src/main/java/com/jian/utdir/ranker/HITS.java
+++ src/main/java/com/jian/utdir/ranker/HITS.java
@@ -3,6 +3,11 @@ package com.jian.utdir.ranker;
 import java.io.File;
 import java.util.Scanner;
 
+/*
+ * this is method deprecated, please use the hits backup method
+ * 
+ */
+
 public class HITS {
 	int[][] adjMatrix;
 	int[] outdegree;
